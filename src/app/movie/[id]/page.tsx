b141b5eb7ee2a96e0eb19196028c75cd78d7eb51
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-05-01 22:07:37 +0300

    vk-work

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -8,26 +8,3 @@ export default async function Movie({ params }: Props) {
   const { id } = await params;
   return <MoviePage movieId={id} />;
 }
-
-
-// import MoviePage from "@/components/MoviePage/MoviePage";
-// import { getMovieId } from "@/api/MovieApi"; // Не забудь импортировать апи
-// import { notFound } from "next/navigation";
-// import { IMovie } from "@/models";
-
-// interface Props {
-//   params: Promise<{ id: string }>;
-//   movie: IMovie;
-// }
-
-// export default async function Movie({ params }: Props) {
-//   const { id } = await params;
-  
-//   const movie = await getMovieId(id);
-
-//   if (!movie) {
-//     notFound();
-//   }
-
-//   return <MoviePage  movie={movie} movieId={id} />;
-// }
--- src/components/HomePage/IntroInfo.tsx
+++ src/components/HomePage/IntroInfo.tsx
@@ -35,21 +35,4 @@ export const IntroInfo = ({ movie }: MovieProps) => {
     </div>
   );
 };
-//    return (
-//   <div className="intro__card-info">
-//     <div
-//       className={`intro__rating ${getRatingClass(Number(movie.tmdbRating))}`}
-//     >
-//       <Icon name="star" width={16} height={15} />
-//       <span className="intro__rating-text ">{movie.tmdbRating.toFixed(1)}</span>
-//     </div>
-//     <span className="intro__card-year">{movie.releaseYear}</span>
-//     <span className="intro__card-genre">
-//       {movie.genres?.map((g) => GENRE_MAP[g] || g).join(", ")}
-//     </span>
-//     <div className="intro__card-runtime">
-//       <strong>{formatRuntime(movie.runtime)}</strong>
-//     </div>
-//   </div>
-// );
-// };
+
