dd7a9ec50b31f4871b87c26bc4818d96ca8dea08
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-05-28 22:30:08 +0300

    pageMovie

---
+++ src/app/movie/[id]/page.tsx
@@ -0,0 +1,30 @@
+
+import MoviePage from "@/components/MoviePage/MoviePage";
+import { getAllMovieIds, getMovieId } from "@/api/MovieApi";
+import { notFound } from "next/navigation";
+import { IMovie } from "@/models";
+
+interface Props {
+  params: Promise<{ id: string }>;
+  movie: IMovie;
+}
+
+export async function generateStaticParams() {
+  const ids = await getAllMovieIds();
+
+  return ids.map((id) => ({
+    id: id,
+  }));
+}
+
+export default async function Movie({ params }: Props) {
+  const { id } = await params;
+
+  const movie = await getMovieId(id);
+
+  if (!movie) {
+    notFound();
+  }
+
+  return <MoviePage movie={movie} movieId={id} />;
+}
