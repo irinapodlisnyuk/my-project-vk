adb03db2f3b9f4feff6ff23318eaf1474cb76588
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-05-28 21:59:39 +0300

    page id movie

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,23 +1,31 @@
 import MoviePage from "@/components/MoviePage/MoviePage";
-import {  getMovieId, getMovieTop } from "@/api/MovieApi";
+import { getAllMovieIds, getMovieId } from "@/api/MovieApi";
 import { notFound } from "next/navigation";
 import { IMovie } from "@/models";
+
 
 
 interface Props {
   params: Promise<{ id: string }>;
   movie: IMovie;
 }
-export const dynamicParams = false; 
+export const dynamicParams = false;
 
 export async function generateStaticParams() {
   try {
-    const ids = await getMovieTop(); 
-    
- console.log(`[Next.js] Генерируем статические страницы для ${ids.length} фильмов.`);
+    const ids = await getAllMovieIds();
 
-    return ids.map((id) => ({
-      id: String(id), 
+    const hardcodedTopIds = ["340929", "561606", "1044042", "575370"];
+
+    // Объединяем списки и убираем дубликаты через Set
+    const finalIds = Array.from(new Set([...ids, ...hardcodedTopIds]));
+
+    console.log(
+      `[Next.js] Успешно создаем страницы для ${finalIds.length} фильмов.`,
+    );
+
+    return finalIds.map((id) => ({
+      id: String(id),
     }));
   } catch (error) {
     console.error("Ошибка при генерации статических страниц фильмов:", error);
@@ -27,7 +35,6 @@ export async function generateStaticParams() {
 
 export default async function Movie({ params }: Props) {
   const { id } = await params;
-
   const movie = await getMovieId(id);
 
   if (!movie) {
@@ -36,7 +43,6 @@ export default async function Movie({ params }: Props) {
 
   return <MoviePage movie={movie} movieId={id} />;
 }
-
 
 // import MoviePage from "@/components/MoviePage/MoviePage";
 // import { getAllMovieIds, getMovieId } from "@/api/MovieApi";
@@ -49,13 +55,12 @@ export default async function Movie({ params }: Props) {
 // }
 
 // export async function generateStaticParams() {
-//   const ids = await getAllMovieIds(); 
-  
+//   const ids = await getAllMovieIds();
+
 //   return ids.map((id) => ({
 //     id: id,
 //   }));
 // }
-
 
 // export default async function Movie({ params }: Props) {
 //   const { id } = await params;
